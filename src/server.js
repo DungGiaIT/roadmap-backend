c3880455d34caa0d2816ba5b97f47febe1471866
DungGiaIT <dungdkso1@gmail.com>
2026-09-25 23:54:23 +0700

    refactor: organize project into backend and frontend

--- src/server.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import app from "./app.js";
-import { prisma } from "./config/db.js";
-
-const PORT = Number(process.env.PORT) || 3000;
-
-await prisma.$connect();
-app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
